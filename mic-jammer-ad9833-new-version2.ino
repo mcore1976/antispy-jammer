f2a5d2e49cd3efc6d630f2b498b445fe3883655f
Adam Loboda <43117081+mcore1976@users.noreply.github.com>
2023-09-23 12:35:09 +0200

    Update mic-jammer-ad9833-new-version2.ino

--- mic-jammer-ad9833-new-version2.ino
+++ mic-jammer-ad9833-new-version2.ino
@@ -320,9 +320,9 @@ void loop() {
 
           // generating INFRASOUND FM modulation over 25kHZ ULTRASOUND CARRIER
           // for android best is random infrasound
-          // j = random (5,25);
+          j = random (28,44);
           // for iPhone best is fixed 45Hz value
-          j = 45;
+          // j = 45;
           // starting frequency for swinging
           i = random(24000,26000);
           // going down with frequency to 24000
@@ -339,7 +339,7 @@ void loop() {
           // for android best is random infrasound
           // j = random (5,25);
           // for iPhone best is fixed 45Hz value
-          j = 45;
+          // j = 45;
           while (i <= 26000)
           {
            i = i + j;
