e0b29893e56993422e9dd49e58fac72187b6c02b
Adam Loboda <43117081+mcore1976@users.noreply.github.com>
2023-09-23 12:42:12 +0200

    Update mic-jammer-ad9833-40kHz-new-version2.ino

--- mic-jammer-ad9833-40kHz-new-version2.ino
+++ mic-jammer-ad9833-40kHz-new-version2.ino
@@ -306,7 +306,7 @@ void setup()
   delay(15);
 
   //set gen to SINE waveform
-  ad9833_set_mode(AD_SINE);
+  ad9833_set_mode(AD_SQUARE);
   // set some center frequency for the start 41KHZ
   ad9833_set_frequency(0, 41000);
 
@@ -322,9 +322,9 @@ void loop() {
 
           // generating INFRASOUND FM modulation over 25kHZ ULTRASOUND CARRIER
           // for android best is random infrasound
-          // j = random (5,25);
+          j = random (28,44);
           // for iPhone best is fixed 45Hz value
-          j = 45;
+          // j = 45;
           // starting frequency for swinging
           i = random(39000,41000);
           // going down with frequency to 39000
@@ -336,12 +336,12 @@ void loop() {
 
           // going up with frequency to 41000 again
           // starting frequency for swinging
-          i = random(39000,41000);
+          // i = random(39000,41000);
           // generating INFRASOUND FM modulation over 25kHZ ULTRASOUND CARRIER
           // for android best is random infrasound
           // j = random (5,25);
           // for iPhone best is fixed 45Hz value
-          j = 45;
+          // j = 45;
           while (i <= 41000)
           {
            i = i + j;
