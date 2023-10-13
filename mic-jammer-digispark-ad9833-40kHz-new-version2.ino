69b75376477b18a241b4ce3babcf4e8fe0e8de5c
Adam Loboda <43117081+mcore1976@users.noreply.github.com>
2023-10-13 16:00:45 +0200

    Update mic-jammer-digispark-ad9833-40kHz-new-version2.ino

--- mic-jammer-digispark-ad9833-40kHz-new-version2.ino
+++ mic-jammer-digispark-ad9833-40kHz-new-version2.ino
@@ -411,10 +411,10 @@ void loop() {
 
           // going up with frequency to 41000 again
           // starting frequency for swinging
-          // i = random(39000,41000);
+          i = random(39000,41000);
           // generating INFRASOUND FM modulation over 40kHZ ULTRASOUND CARRIER
           // for android best is random infrasound
-          // j = random (5,25);
+          j = random (28,44);
           // for iPhone best is fixed 45Hz value
           // j = 45;
           while (i <= 41000)
