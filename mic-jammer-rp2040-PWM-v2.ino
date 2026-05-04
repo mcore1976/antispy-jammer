9281d8fc7b76ece8da56d41f93c1b4e7352e5c9d
Adam Loboda <43117081+mcore1976@users.noreply.github.com>
2026-05-04 18:18:17 +0200

    Update mic-jammer-rp2040-PWM-v2.ino
    updated delaymicroseconds for completely silent jamming

--- mic-jammer-rp2040-PWM-v2.ino
+++ mic-jammer-rp2040-PWM-v2.ino
@@ -102,7 +102,7 @@ void loop() {
         if (j == 0) j = 1;  // safeguard
        
        // you may want to adjust these
-       delayMicroseconds(20);
+       delayMicroseconds(140);
     }
 
     // swiping up 24kHz -> 26kHz
@@ -130,6 +130,6 @@ void loop() {
         if (j == 0) j = 1; // safeguard
 
        // you may want to adjust these
-        delayMicroseconds(20);
+        delayMicroseconds(140);
     }
 }
