0a45f353a196b85c7d370082a799af51faa17138
Adam Loboda <43117081+mcore1976@users.noreply.github.com>
2026-07-13 15:59:24 +0200

    New approach to jamming. completely not audible, the "chopper" version which tricks AGC in smartphones

--- mic-jammer-rp2040-PWM-v14.ino
+++ mic-jammer-rp2040-PWM-v14.ino
@@ -1,5 +1,6 @@
 /*
     wearable microphone jammer
+
   
     Version based on original concept &  development :
     https://github.com/y-x-c/wearable-microphone-jamming
@@ -20,6 +21,7 @@
 #include <Arduino.h>
 #include "hardware/pwm.h"   // Raspberry PICO SDK library
 #include "hardware/clocks.h" // For function clock_get_hz
+#include "hardware/adc.h"
 
 // pin #1 for GP1 on RP2040-zero board
 const int PWM_PIN = 1;
@@ -53,7 +55,7 @@ void update_frequency_safe(uint32_t freq) {
     // but setting up to 70% for highest voltage on the coil
     // the higher this value is the more DC current is pumped into coil by the MOSFET
     // both MOSFET and coil will get hotter 
-    uint16_t level = (uint16_t)((wrap + 1) * 60 / 100);
+    uint16_t level = (wrap + 1) * 60UL / 100UL;
     pwm_set_chan_level(slice_num, channel, level);
 }
 
@@ -75,25 +77,49 @@ void setup() {
 
     // Setting central frequency for piezo
     update_frequency_safe(BASE_CARRIER);
+
+
+    // Randomization seed with ADC input
+    adc_init();
+    adc_gpio_init(26); 
+    adc_select_input(0); // channel 0 (GP26) as an input
+
+    uint32_t seed = 0;
+    for (int i = 0; i < 16; i++) {
+        seed = (seed << 2) ^ adc_read();
+        delayMicroseconds(50); };
    
-    randomSeed(1234);
+    randomSeed(seed);
 }
 
 
 void loop() {
+    unsigned long startMicros, randomfrequencyDurationMicros;
+    uint16_t randomfrequency ;
+
+    // randomization seed with use of ADC input pin 0 - GP26
+    uint32_t seed = 0;
+    for (int i = 0; i < 16; i++) {
+        seed = (seed << 2) ^ adc_read();
+        delayMicroseconds(50);  };
+   
+    randomSeed(seed);
+  
     // Here we pick a random INFRASOUND frequency for FM modulation
-    uint16_t randomfrequency = random(1,15);
-    
+    //uint16_t randomfrequency = random(1,25);
+    randomfrequency = random(20,50);
+        
     // Random human speech wovel frequency to trick smartphones DSP
     //int vowelIdx = random(0, VOWEL_COUNT);
     //uint16_t randomfrequency = formants[vowelIdx];
 
     // Here we pick random time of this frequency duration
     // you may play with this range to have better jamming results on phones mic
-    unsigned long randomfrequencyDurationMicros = random(3, 7) * 1000;
+    //unsigned long randomfrequencyDurationMicros = random(3, 7) * 1000;
+    randomfrequencyDurationMicros = random(1, 10) * 1000;
 
     // We gather starting time in microseconds for the precision of FM modulation
-    unsigned long startMicros = micros();
+    startMicros = micros();
 
     // Delta-time for the loop interval = 0.000040 sec  40 microseconds - has to match delay at the end of loop
     const float dt = 0.000040;
@@ -102,18 +128,18 @@ void loop() {
 
          // We calculate FM phase increase during this loop cycle for previusly selected modulation frequency
          //  dPhi = 2 * PI * f * dt
-         float phase_increment = 2.0 * PI * randomfrequency * dt;
+         float phase_increment = 6.28 * randomfrequency * dt;
 
          // Accumulating phase value so the FM modulation has no glithces 
          fm_phase += phase_increment;
 
          // Adjusting phase so it would match range 0 - 2PI
-         if (fm_phase >= 2.0 * PI) fm_phase -= 2.0 * PI;
+         if (fm_phase >= 6.28 ) fm_phase = fm_phase - 6.28;
 
         // FM modulation to create the sound upon bearer piezo frequency 
         // Weight 1000 adjusted to fit within piezo acceptable frequency  
         // piezo range is  24000 - 26000 Hz (25000 +/- 1000 Hz)
-         long fm_offset = (sin(fm_phase) * 1000);
+         long fm_offset = (sin(fm_phase) * 3000);
          uint32_t current_freq = BASE_CARRIER + fm_offset;
         
         // Update the frequency
