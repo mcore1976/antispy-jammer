22f5a7043de89a7b1d280bae98f36cbd94ee8829
Adam Loboda <43117081+mcore1976@users.noreply.github.com>
2026-07-13 09:25:23 +0200

    New approach to jamming. completely not audible, the "chopper" version which tricks AGC in smartphones

---
+++ antispam-jammer-H-Bridge-RP2040-PWM-IR2104-IRF4115-v14-dead-tim.ino
@@ -0,0 +1,99 @@
+/*
+    Wearable microphone jammer - Vowel Formant Modulation Version (Strict 24kHz-26kHz)
+    Target: RP2040-ZERO / RP2040-ONE
+    Core: Earle Philhower RP2040 Core
+    (C) Adam Loboda 2026
+    version 13 - FM random modulation (sinusoidal wave)  for bobble sounds
+*/
+
+#include <Arduino.h>
+#include "hardware/pwm.h"  
+#include "hardware/clocks.h"
+
+const int PIN_NORMAL = 0;    // GP0
+const int PIN_INVERTED = 1;  // GP1
+uint slice_num;
+
+
+// Middle of piezzo transducers resonance frequency
+const uint32_t BASE_CARRIER = 25000; 
+
+// Trackig Sinusoidal modulation signal phase for seamless FM modulation
+float fm_phase = 0.0;
+
+// Human speech formants defintiion for [A, E, I, O, U]
+const uint16_t formants[] = {730, 530, 270, 570, 300, 1090, 1840, 840, 870};
+const int VOWEL_COUNT = 9;
+
+void update_frequency_safe(uint32_t freq) {
+    uint32_t sys_clk = clock_get_hz(clk_sys);
+    
+    uint32_t wrap = sys_clk / (2 * freq);
+    pwm_set_wrap(slice_num, wrap);
+
+    // set the PWM duty level
+    uint32_t level = (wrap / 2.1); 
+ 
+    pwm_set_chan_level(slice_num, PWM_CHAN_A, level);
+    pwm_set_chan_level(slice_num, PWM_CHAN_B, wrap - level);
+}
+
+void setup() {
+    gpio_set_function(PIN_NORMAL, GPIO_FUNC_PWM);
+    gpio_set_function(PIN_INVERTED, GPIO_FUNC_PWM);
+  
+    slice_num = pwm_gpio_to_slice_num(PIN_NORMAL);
+
+    pwm_config config = pwm_get_default_config();
+    pwm_config_set_phase_correct(&config, true);
+    pwm_config_set_output_polarity(&config, false, true); 
+    pwm_config_set_clkdiv(&config, 1.0f);
+    pwm_init(slice_num, &config, true);
+
+    update_frequency_safe(BASE_CARRIER);
+    randomSeed(1234);
+}
+
+void loop() {
+    // Here we pick a random INFRASOUND frequency for FM modulation
+    uint16_t randomfrequency = random(1,15);
+    
+    // Random human speech wovel frequency to trick smartphones DSP
+    //int vowelIdx = random(0, VOWEL_COUNT);
+    //uint16_t randomfrequency = formants[vowelIdx];
+
+    // Here we pick random time of this frequency duration
+    // you may play with this range to have better jamming results on phones mic
+    unsigned long randomfrequencyDurationMicros = random(3, 7) * 1000;
+
+    // We gather starting time in microseconds for the precision of FM modulation
+    unsigned long startMicros = micros();
+
+    // Delta-time for the loop interval = 0.000040 sec  40 microseconds - has to match delay at the end of loop
+    const float dt = 0.000040;
+    
+    while (micros() - startMicros < randomfrequencyDurationMicros) {
+
+         // We calculate FM phase increase during this loop cycle for previusly selected modulation frequency
+         //  dPhi = 2 * PI * f * dt
+         float phase_increment = 2.0 * PI * randomfrequency * dt;
+
+         // Accumulating phase value so the FM modulation has no glithces 
+         fm_phase += phase_increment;
+
+         // Adjusting phase so it would match range 0 - 2PI
+         if (fm_phase >= 2.0 * PI) fm_phase -= 2.0 * PI;
+
+        // FM modulation to create the sound upon bearer piezo frequency 
+        // Weight 1000 adjusted to fit within piezo acceptable frequency  
+        // piezo range is  24000 - 26000 Hz (25000 +/- 1000 Hz)
+         long fm_offset = (sin(fm_phase) * 1000);
+         uint32_t current_freq = BASE_CARRIER + fm_offset;
+        
+        // Update the frequency
+        update_frequency_safe(current_freq);
+
+        // time for 1 PWM cycle - should match value used for phase calculation
+        delayMicroseconds( dt * 1000000);
+    }
+}
